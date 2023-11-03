93b1a1800b0ebb33260f430338d897e328d836ba
vanestw <125222365+vanestw@users.noreply.github.com>
2023-11-03 14:51:08 -0600

    fuck you

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+using namespace 
 int main() {
+    cout << "fuck you" << endl;
+    vector<string> fuckYou;
+
+    for(size_t i = 0; i < 10; i++){
+        fuckYou.emplace_back("fuck you").at(i);
+    }
 }
+
