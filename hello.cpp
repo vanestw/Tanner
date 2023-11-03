8744c4d6cb3e0c1f725609ddec6fbce7034510dc
tannervanes <tannervanes@gmail.com>
2023-11-03 16:16:27 -0600

    11/3/23

--- hello.cpp
+++ hello.cpp
@@ -1,21 +1,2 @@
-#include <iostream>
-#include <string>
-#include <vector>
 
-using namespace std;
-
-int main() {
-    cout << "fuck you" << endl;
-    vector<string> fuckYou;
-
-    for (uint i = 0; i < 10; i++) {
-        fuckYou.emplace_back("fuck you");
-    }
-    for(const auto& elm: fuckYou){
-        cout << elm << endl;
-
-    }
-
-    // WOOOOOOO WHAT IS GIT AND TANNED TANNER IN THE FLAME FLANNER
-}
 
